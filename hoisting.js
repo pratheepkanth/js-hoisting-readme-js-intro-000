459dce84a67ab3a0bb6b4d698c9e0634b7b1bcbc
pratheevan kasirajah <pratheepkanth@yahoo.com>
2018-07-18 20:47:46 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -1,11 +1,12 @@
-var lyric = "maybe";
+
 function callMe() {
+  var lyric;
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
+  lyric = "maybe";
 }
 
 
